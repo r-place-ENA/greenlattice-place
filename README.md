96b786dce9fb06953c30c32ffb47e3a05105c685
PizzaRalsei <86186235+pizzaralsei@users.noreply.github.com>
2022-04-04 17:16:40 -0300

    Updated some artwork goals

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Green Lattice Overlay for r/place
+# Green Lattice Overlay and ENA for r/place
 
 
 
@@ -14,7 +14,7 @@ Install one of the following extensions for your browser.
 
 Install the Userscript:
 
-[lattice.user.js](https://raw.githubusercontent.com/jcb1032/greenlattice-place/main/lattice.user.js)
+[lattice.user.js](https://raw.githubusercontent.com/pizzaralsei/greenlattice-place/main/lattice.user.js)
 
 #
 
@@ -26,11 +26,11 @@ Sorry, the script is unavailable for mobile users.
 
 The overlay will be automatically updated by our team, and you shouldn't have to change anything.
 
-However, sometimes your browser will cache the overlay image for faster loading times. You can go to [this link](https://raw.githubusercontent.com/jcb1032/greenlattice-place/main/lattice_overlay.png) and click the refresh button (F5) while holding shift to re-cache the image.
+However, sometimes your browser will cache the overlay image for faster loading times. You can go to [this link](https://raw.githubusercontent.com/pizzaralsei/greenlattice-place/main/lattice_overlay.png) and click the refresh button (F5) while holding shift to re-cache the image.
 
 ### Contributing
 
-1. Open `lattice_full.png` in photoshop, make changes, and save over the original, with the same name
+1. Open `lattice_full.png` in your image editor, make changes, and save over the original, with the same name
 2. Run the `dithering.py` script* in the same folder as the saved `lattice_full.png` image
 3. Upload `lattice_full.png` and the newly generated `lattice_overlay.png` to github
 
@@ -47,10 +47,16 @@ The r/osuplace team for the original code: _oralekin_ & _exdeejay_
 
 The r/greenlattice team for our modifications: [_artillect#8709_](https://github.com/artillect), _jcb#1032_, & [_jumpinglizard#4404_](https://github.com/BlueRedBlueYellow)
 
+The r/enaplace team for ENA modifications: _Gezin#5078_, [_pizzaralsei#9465_](https://github.com/pizzaralsei), & [_AZMPlay#9283_](https://github.com/AZMCode)
+
 ### Links
 
 [r/place](https://www.reddit.com/r/place)
 
 [r/greenlattice](https://www.reddit.com/r/greenlattice)
 
+[r/enaplace](https://www.reddit.com/r/enaplace)
+
 [GL Discord](https://discord.gg/D38szSvvX3)
+
+[ENA Discord](https://discord.gg/Tg678DNp)
--- lattice.user.js
+++ lattice.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
-// @name         Green Lattice Overlay
+// @name         Green Lattice-ENA Overlay
 // @namespace    https://reddit.com/r/greenlattice
-// @version      0.2
+// @version      0.5
 // @description  overlay for r/place
-// @author       FOR OSU: oralekin, exdeejay | FOR GL: artillect#8709, jcb#1032, jumpinglizard#4404
+// @author       FOR OSU: oralekin, exdeejay | FOR GL: artillect#8709, jcb#1032, jumpinglizard#4404 | FOR ENA: Gezin#5078, pizzaralsei#9465, AZMPlay#9283
 // @match        https://hot-potato.reddit.com/embed*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
 // @grant        none
-// @updateURL    https://github.com/jcb1032/greenlattice-place/raw/main/lattice.user.js
-// @downloadURL  https://github.com/jcb1032/greenlattice-place/raw/main/lattice.user.js
+// @updateURL    https://github.com/pizzaralsei/greenlattice-place/raw/main/lattice.user.js
+// @downloadURL  https://github.com/pizzaralsei/greenlattice-place/raw/main/lattice.user.js
 // ==/UserScript==
 
 if (window.top !== window.self) {
@@ -21,8 +21,8 @@ if (window.top !== window.self) {
         .shadowRoot.children[0].appendChild(
           (function () {
             const i = document.createElement("img");
-            i.src = "https://raw.githubusercontent.com/jcb1032/greenlattice-place/main/lattice_overlay.png";
-            i.style = "position:absolute;left:0;top:0;image-rendering:pixelated;width:2000px;height:1000px;";
+            i.src = "https://raw.githubusercontent.com/pizzaralsei/greenlattice-place/main/lattice_overlay.png";
+            i.style = "position:absolute;left:0;top:0;image-rendering:pixelated;width:2000px;height:2000px;";
             i.setAttribute("data-display", "1");
 
             // toggle on key press
@@ -37,10 +37,10 @@ if (window.top !== window.self) {
             window.history.oldReplaceState = window.history.replaceState;
             window.history.replaceState = function (a, b, c, d) {
               let coordsEl = document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-coordinates")[0].shadowRoot.children[0];
-              let zoomLevel = coordsEl.textContent.replace(/^ \(\d+,\d+\) (\d\.\d)x $/, "$1");
+              let zoomLevel = coordsEl.textContent.replace(/^ \(\d+,\d+\) (\d\.\d+)x $/, "$1");
 
               // disable when you're zoomed out
-              if (zoomLevel == "0.1" || zoomLevel == "0.2") {
+              if (parseFloat(zoomLevel) <= 0.2) {
                 i.style.display = "none";
               } else {
                 if (i.getAttribute("data-display") == "1") i.style.display = "block";
